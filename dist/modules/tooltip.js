1bdccfd6505f0f25e21c5a8e3c0bb9ff1963a43d
Richard Zschech <richard.zschech@tradingscreen.com>
2015-07-01 15:21:18 -0400

    chore(release): cut the v2.2.6 release

--- dist/modules/tooltip.js
+++ dist/modules/tooltip.js
@@ -419,13 +419,16 @@ angular.module('mgcrea.ngStrap.tooltip', [ 'mgcrea.ngStrap.helpers.dimensions' ]
       }
       function hideIfNotChildEvent(event) {
         var node = event.target;
-        while (node && node !== $body[0]) {
+        while (node) {
           if (node === tipElement[0]) {
+            return;
+          }
+          if (node === $body[0]) {
+            $tooltip.hide();
             return;
           }
           node = node.parentNode;
         }
-        $tooltip.hide();
       }
       function getPosition($element) {
         $element = $element || (options.target || element);
