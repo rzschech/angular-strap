0814e365e3b12521e566597d87f6266128aa9b08
Richard Zschech <richard.zschech@tradingscreen.com>
2015-07-01 15:20:37 -0400

    feature(*): don't close tooltip if click is on removed element

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -552,13 +552,16 @@ angular.module('mgcrea.ngStrap.tooltip', ['mgcrea.ngStrap.core', 'mgcrea.ngStrap
 
         function hideIfNotChildEvent (event) {
           var node = event.target;
-          while (node && node !== $body[0]) {
+          while (node) {
             if (node === tipElement[0]) {
+              return;
+            }
+            if (node === $body[0]) {
+              $tooltip.hide();
               return;
             }
             node = node.parentNode;
           }
-          $tooltip.hide();
         }
 
         // Private methods
